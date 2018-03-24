8e8ac305fe29b041b88c239f61f75f3ee58f57f0
Paul Huske <pbhuske@gmail.com>
2018-03-23 16:10:20 -0800

    outline of the functions

--- headers/Shape.h
+++ headers/Shape.h
@@ -1,2 +1,15 @@
 
 //THis is the Shape Base class that Circle, Rectangle and Regular Polygon Inherit from
+class Shape
+{
+
+
+public:
+
+virtual void Rotated(Shape shape, int rotation_Angle);
+virtual void Scaled(Shape shape, double x_Scale, double y_Scale);
+void Layered();
+void Vertical();
+void Horizontal();
+
+};
