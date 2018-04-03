56a81216bfcefc3e3f0eb80b8265afef50272f8a
Paul Huske <pbhuske@gmail.com>
2018-04-03 14:47:02 -0800

    Add files via upload
    added the rotation now works for 90,180 270 degrees which was all that was needed.  My mistake to try for a more general solution

--- apothemCalc.cpp
+++ apothemCalc.cpp
@@ -40,43 +40,157 @@ std::pair<int, int> starting_Point(float x, float y, float s, int n) {  //Defaul
 	pair<int, int> point;
 	float pi = 3.14159265;
 	float apothem = (s / 2) / tan(pi / n);
-	if (n % 2 == 1)
-	{
-		pair<int, int> box = bounding_Box(n, s);
-		apothem = box.second / 2;
-	}
 	point.first = (int)(x - s / 2);
 	point.second = (int)(y - apothem);
 
 	return point;
 }
+// takes the original bounding box for the unrotated shape and scales it to fit the rotated shape
+//Only works for 90, 180 or 270 degree rotations
+pair<int, int> rotate_Box(pair<int,int> bBox, int degree, int n, double side)
+{
+
+	if (degree == 90 || degree == 270)
+	{
+		int temp = bBox.first;
+		bBox.first = bBox.second;
+		bBox.second = temp;
+		return bBox;
+	}
+	else
+		return bBox;
+}
+	/*
+	float pi = 3.14159265;
+	int d = degree % (360 / n);
+	double radian = (d / 180.0) * 3.1415;
+	float apothem = (side / 2) / tan(pi / n);
+	int w = bBox.first;
+	int h = bBox.second;
+	double r = side / 2 / sin(3.14 / n); // polygon radius
+
+	int width = 0;
+	int height = 0;
+
+	if (n % 2 == 0 && n %4 != 0)
+	{
+
+	
+
+	//double diam = sqrt(h*h + side*side);
+	//double radius = diam / 2 - h / 2;
+
+	double amplitude = w - h ;
+	double frequency = pi*n / 360.0*d;
+	height = h + amplitude*sin(frequency);
+	width = h + amplitude * cos(frequency);
+	//  height = (int)(r*sin(2*3.14/(2*n)*radian) + h);
+	//  width = (int)(r*sin(2 * 3.14 / (2 * n)*radian) + w);
+	}
+	else if (n % 2 == 0) { //square style
+		double amplitude = r - apothem;
+		double frequency = pi*n / 360.0*d;
+		height = h + amplitude * 2 * sin(frequency);
+		width = height;
+	}
+
+	else { //odd
+		/*
+		radian = (360.0 / n) / 180.0 * 3.14;
+		r = sqrt((side*side)/(2-2*cos(radian)));
+		 height = 2 * (int)r;
+		width = 2 * (int)r;
+		
+		double amplitude = w - h;
+		double frequency = pi*4*n / 360.0*d;
+		height = h + amplitude  * sin(frequency);
+		width = h + amplitude  * cos(frequency);
+		*/
+	
+
+
+//The New Starting point for rotated shapes so that the Shape remains centered on he current_point
+pair<int, int> start_Rotate(pair<int, int> current_point, pair<int, int> start_point, int degree, double side, int n)
+{
+	
+	int d = degree % (360 / n);
+	
+	double radian = (d / 180.0) * 3.1415;
+	double r = side / 2 / sin(3.14 / n); // polygon radius
+	pair<int, int> new_coord = std::make_pair(0, 0);
+
+	int xs = start_point.first;
+	int ys = start_point.second;
+
+	int x = current_point.first;
+	int y = current_point.second;
+
+	xs = xs - x;
+	ys = ys - y;
+
+	double x2 = xs * cos(radian) - ys * sin(radian);
+	double y2 = xs * sin(radian) + ys*cos(radian);
+
+	new_coord.first = x + (int)(x2);
+	new_coord.second = y + (int)y2;
+	return new_coord;
+}
+
 
 int main()
 {
 
 	 box b;
 	 box a = box(5, 3);
-	 box c = box(10, 10);
+	 box c = box(100, 100);
 	 box d = box(1, 25);
 	 box g = box(11, 4);
 
-	 pair<int,int> answer = b.maxD({ a,c,d,g });
-	cout << a.getHeight() << "  " << a.getWidth() << endl;
-	cout << answer.first << "\t" << answer.second << endl;
-	/*
+	// pair<int,int> answer = b.maxD({ a,c,d,g });
+	 int degree = 30;
+	 pair<int, int> current = std::make_pair(300, 300);
+
+	
+
+	 double h = c.getHeight();
+	 double w = c.getWidth();
+	 double diam = sqrt(h*h + w*w);
+	 double radian = degree * 3.14159 / 180;
+	 double side = 100;
+	
+	 cout << " Bounding Box with no Rotation \n";
+	 for (int n = 3; n < 12; n++)
+	 {
+		 pair<int, int> bBox = bounding_Box(n, 100);
+		 cout << "Sides " << n << " the Width is " << bBox.first << " the Height is " << bBox.second << endl;
+	 }
+
+	 cout << " Starting Point with no Rotation \n";
+	 for (int n = 3; n < 12; n++)
+	 {
+		 pair<int, int> bBox = starting_Point(current.first, current.second, 100, n);
+		 cout << "Sides " << n << " X coord is " << bBox.first << " y coord is " << bBox.second << endl;
+	 }
+
+	cout<< "\n Bounding box with Rotation \n";
+
 	for (int n = 3; n < 12; n++)
 	{
 		pair<int, int> bBox = bounding_Box(n, 100);
-		cout <<"Sides " << n << " the Width is "<< bBox.first << " the Height is " << bBox.second << endl;
+		pair<int, int> bBoxr = rotate_Box(bBox, degree, n, side);
+		cout <<"Sides " << n << " the Width is "<< bBoxr.first << " the Height is " << bBoxr.second << endl;
 	}
-
-	cout << " \n\n Starting Point \n\n ";
+	
+	cout << " \n Starting Point with Rotation \n ";
 	for (int n = 3; n < 12; n++)
 	{
-		pair<float, float> bBox = starting_Point(300, 300, 100, n);
-		cout << "Sides " << n << " the x Coord is " << bBox.first << " the y coord is " << bBox.second << endl;
+		pair<float, float> bBox = bounding_Box(n, 100);
+	//	cout << "Sides " << n << " the x Coord is " << bBox.first << " the y coord is " << bBox.second << endl;
+		auto start = starting_Point(300, 300, 100, n);
+		auto check = start_Rotate(current, start, degree, side, n);
+			cout << "Sides " << n << " the x Coord is " << check.first << " the y coord is " << check.second << endl;
 	}
-	*/
+	
     return 0;
 }
 
